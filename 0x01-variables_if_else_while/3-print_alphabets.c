8d347c68cd37ebe12bd8e66b7774bf2fe9b7fafe
DonovanSte <steynfaardtd@gmail.com>
2023-02-17 11:09:32 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase || uppercase || new line
+ *
+ * only allowed to use putchar (count 3 max)
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	/* prints alphabet in lowercase */
+	char c;
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	/* Prints alphabet in uppercase */
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
