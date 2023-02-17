a7302762c781add7518c13dff258d131f17c3299
DonovanSte <steynfaardtd@gmail.com>
2023-02-17 09:34:58 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
 *Print random positive or negative number
 *identify wheter it is a positve or negative number
-*if the number is greater than 0: is positive                                                                                                                                              
+*if the number is greater than 0: is positive
 *if the number is 0: is zero
 *if the number is less than 0: is negative
 *Return: always return 0
