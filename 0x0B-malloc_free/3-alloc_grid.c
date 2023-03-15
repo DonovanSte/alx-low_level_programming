1d90d149b86ad68ba73d56187e6686a0506c017c
DonovanSte <steynfaardtd@gmial.com>
2023-03-15 23:06:28 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,25 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * alloc_grid - eturns a pointer to a 2 dimensional array of integers
- * @width: with of 2D array
- * @height: heigt of 2D array
- * Return: NULL if 0 or neagtive
+ * alloc_grid - creates 2D array of ints
+ * @width: width of 2D array
+ * @height: height of 2D array
+ * Return: pointer to 2D array or NULL if failure
  */
 int **alloc_grid(int width, int height)
 {
+	int **arr;
 	int i, j;
-	int *ptr = NULL;
 
-	if (width <= 0 || height <= 0)
+	if (width < 1 || height < 1)
 		return (NULL);
-
-	ptr = (int *)malloc(width * height * sizeof(int));
-
-	for (i = 0; i < width * height; i++)
-		;
-
-	return (ptr);
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	return (arr);
 }
