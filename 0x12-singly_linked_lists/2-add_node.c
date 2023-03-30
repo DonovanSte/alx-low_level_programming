35cd9f66fdb7c1e1fb5e9409c63184c7be6b6fdb
DonovanSte <steynfaardtd@gmial.com>
2023-03-30 21:35:06 +0200

    Ammended lists.h && added add_node function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -35,7 +35,7 @@ list_t *add_node(list_t **head, const char *str)
 		len = 0;
 	}
 	/* Set next field and pointer to new_node */
-	new_node->len = len;
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
