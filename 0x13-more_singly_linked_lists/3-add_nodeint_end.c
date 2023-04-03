fb9ecb1bc5730674d745ab947eacf2d61f8c783b
DonovanSte <steynfaardtd@gmial.com>
2023-04-03 21:26:17 +0200

    Fourth push, Added added Node at End, updated lists.h & README.md

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the pointer to the head of the list
+ * @str: string to be stored in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = NULL;
+	listint_t *current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current->next != NULL)
+		current = current->next;
+
+	current->next = new_node;
+	return (new_node);
+	
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+}
