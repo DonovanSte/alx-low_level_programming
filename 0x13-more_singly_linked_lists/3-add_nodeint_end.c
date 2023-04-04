6d52edf390a545519b9687927acca658e9e778c8
DonovanSte <steynfaardtd@gmial.com>
2023-04-04 21:29:25 +0200

    removed loop too free node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -32,11 +32,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	current->next = new_node;
 	return (new_node);
-
-	while (*head != NULL)
-	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
-	}
 }
