1ef3fee90bcded2cfbee661bf6889f2c5b8b6cdd
DonovanSte <steynfaardtd@gmial.com>
2023-04-11 17:44:48 +0200

    Added main.h, 0-binary_to_uint.c && updated README.md

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+/* Libraries && Header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/* Function prototypes */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* LISTS_H */
