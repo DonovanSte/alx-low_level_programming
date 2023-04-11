825d1c544bd32a94b36b974ee515f7bdc5d7c639
DonovanSte <steynfaardtd@gmial.com>
2023-04-11 18:39:38 +0200

    Added 2-get_bit.c && updated main.h, README.md

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,8 @@
  * @n: the number to get the bit from
  * @index: the index of the bit to get
  *
- * Return: the value of the bit at the specified index, or -1 if an error occurred
+ * Return: the value of the bit at the specified index,
+ *  or -1 if an error occurred
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
