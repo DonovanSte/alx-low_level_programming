b67a0149cb2f30b8866dc78c608ce5dd4c318342
DonovanSte <steynfaardtd@gmial.com>
2023-06-07 23:53:54 +0200

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,12 +8,12 @@
  */
 int is_palindrome(char *s)
 {
-	int len = strlen(s),i,j;
+	int len = strlen(s), i, j;
 
-	for (i = o, j = len -1; i < j; i++, j--)
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		if (s[i] != s[j])
 			return (0);
 	}
-	retrun (1);
+	return (1);
 }
