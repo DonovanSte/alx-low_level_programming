fc6dab4cf438e2c15d73d2347e1be513e6277d75
DonovanSte <steynfaardtd@gmial.com>
2023-06-08 00:06:10 +0200

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -10,13 +10,13 @@
  */
 int is_palindrome_recursive(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
+	if (start >= end)
+		return (1);
 
-    if (s[start] != s[end])
-        return 0;
+	if (s[start] != s[end])
+		return (0);
 
-    return is_palindrome_recursive(s, start + 1, end - 1);
+return (is_palindrome_recursive(s, start + 1, end - 1));
 }
 
 /**
@@ -27,7 +27,7 @@ int is_palindrome_recursive(char *s, int start, int end)
  */
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
+	int len = strlen(s);
 
-    return is_palindrome_recursive(s, 0, len - 1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
