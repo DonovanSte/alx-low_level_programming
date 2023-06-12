8f1443f8265c254fa15d9ce17dd51a62a7872d4a
DonovanSte <steynfaardtd@gmial.com>
2023-06-12 20:29:15 +0200

    Main Header File

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* FUNCTION PROTOTYPES */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* MAIN_H */
