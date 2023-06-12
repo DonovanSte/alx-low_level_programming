a9f31bc5e5741fbd4a06e67d357bad79e23eb59d
DonovanSte <steynfaardtd@gmial.com>
2023-06-12 20:45:16 +0200

    Added a function _strdup

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,9 +5,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* FUNCTION PROTOTYPES */
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
